58b1edccc2c7d2cae301e950f97476716862cd62
etcheekes <echeekes@gmail.com>
2024-12-22 12:56:38 +0000

    run_query function to run simple queries

--- imdb_pipeline/code/database_processing.py
+++ imdb_pipeline/code/database_processing.py
@@ -28,35 +28,52 @@ def rename_file(file_to_rename, new_name):
         # report error if error occurs
         print(f"error renaming file: {e}")
 
-def query_read(db_path, query_string):
+def run_query(db_path, query_string):
 
     try:
         conn = sqlite3.connect(db_path)
         cursor = conn.cursor()
+        # execute query string
         cursor.execute(query_string)
-        results = cursor.fetchall()
-        return results
+        # identify if read query
+        read_query = "SELECT * FROM title_ratings".lower().strip().startswith("select")
+        if read_query:
+            # if read query
+            results = cursor.fetchall()
+            return results
+        else:
+            # if any query that modifies the database
+            conn.commit()
     except sqlite3.Error as e:
         print(f"Error: {e}")
         return None
-    
+    finally:
+        conn.close()
+     
 # variables
 old_db_name = f"{processed_file_dir}/{raw_database_name}"
 new_db_name = f"{processed_file_dir}/{processed_database_name}"
 
+# rename db file if specified name doesn't exist
 if os.path.exists(new_db_name) != True:
     move_file_single(raw_database_name, processed_file_dir, raw_file_dir, ".db")
     print("db file transferred")
     rename_file(old_db_name, new_db_name)
 else:
     print("db file already exists")
+
+
 # function to run queries
-# def execute_query(db, query):
-#     print("test")
+query = "SELECT * FROM title_ratings"
+test = run_query(new_db_name, query)
+print(test)
 
-# create copy of database
 
-# replace /n with appropriate missing value
+# check structure: 1) identify issue, 2) update issue, 3) read query to check issue is fixed
+
+# replace /n with NULL
+
+
 
 # check primary key and foregin key is unique in each table
 
